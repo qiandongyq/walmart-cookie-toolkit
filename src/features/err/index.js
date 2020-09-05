4e2f95ae96d56fa662e0bc3d01525ffa4543d1e7
Dong Qian <qiandongyq@gmail.com>
2020-09-05 00:06:58 -0400

    change wording in err feature

--- src/features/err/index.js
+++ src/features/err/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Box, Heading } from '@chakra-ui/core';
+import { Box, Heading, Text } from '@chakra-ui/core';
 
 export const ERR = () => (
   <Box py={5}>
     <Heading as="h3" size="sm">
-      COMING SOON!
+      NOT STARTED YET!
     </Heading>
+    <Text>Still in investigation stage</Text>
   </Box>
 );
